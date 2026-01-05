e02f3000f800a3edee768f2a8a110ccf1e32c4d9
Jeremy Dubrulle <jeremy.dubrulle@umons.ac.be>
2026-01-05 13:44:16 +0100

    refactor(driver): remove unused radio constants
    Signed-off-by: Jeremy Dubrulle <jeremy.dubrulle@umons.ac.be>

--- dot15d4-driver/src/radio/constants.rs
+++ dot15d4-driver/src/radio/constants.rs
@@ -1,15 +1,7 @@
 #![allow(dead_code)]
 
-/// The number of consecutive lost beacons that will cause the MAC sublayer of a
-/// receiving device to declare a loss of synchronization.
-pub const A_MAX_LOST_BEACONS: u8 = 4;
 /// The maximum size of an MPDU, in octets, that can be followed by a SIFS
 /// period.
 pub const A_MAX_SIFS_FRAME_SIZE: u16 = 18;
 /// The number of slots contained in any superframe.
 pub const A_NUM_SUPERFRAME_SLOTS: u8 = 16;
-
-// Constants from section 10.25.11, Table 10-121
-/// The number of superframes in which a GTS descriptor exists in the beacon
-/// frame of the PAN coordinator.
-pub const A_GTS_DESC_PERSISTENCE_TIME: u8 = 4;
